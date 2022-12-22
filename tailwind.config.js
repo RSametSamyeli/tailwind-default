4cd0f51452dc6435ca060d6bd5997c9cff68b7fe
Samet Samyeli <91800314+RSametSamyeli@users.noreply.github.com>
2022-12-22 10:34:12 +0300

    Case tamamlandı.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,47 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '2.5rem',
+        sm: '1rem',
+        lg: '1rem',
+        xl: '2.5rem',
+      }
+    },
+    extend: {
+      fontFamily: {
+        'sans': ['Source Sans Pro', 'sans-serif'],
+      },
+
+      flex: {
+        'half': ' 0 0 50%',
+      },
+
+      colors:{
+        'primary': '#B40A17',
+        'primary-accent': '#A20915',
+        'secondary': '#0D52C1',
+        'secondary-light': '#CFDCF3',
+        'secondary-lighter': '#E7EEF9',
+        'high-emphasis': '#262626',
+        'medium-emphasis': '##595959',
+        'success-lighter': '#EAF6F0',
+        'success-accent': '##26905E',
+        'warning-accent': '#D97701',
+        'light': '#EFEFEF',
+        'border': '#CFD8DC',
+        'gradient-burgundy': '#B40A17',
+        'gradient-red': '#D13F4A',
+        'warning-lighter': '#FEF3E6',
+        'warning-accent': '#D97701',
+
+      },
+      shadow:{
+        'card': '0px 6px 20px -4px rgba(0, 0, 0, 0.1)',
+      }
+    },
   },
   plugins: [],
 }
